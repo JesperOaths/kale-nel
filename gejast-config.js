957edf78beffd375823cd3ead1bd2e8e8e0c18d6
JesperOaths <150788310+JesperOaths@users.noreply.github.com>
2026-03-24 15:48:25 +0100

    Add files via upload

---
+++ gejast-config.js
@@ -0,0 +1,7 @@
+window.GEJAST_CONFIG = {
+  VERSION: 'v73',
+  SUPABASE_URL: 'https://uiqntazgnrxwliaidkmy.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_rBDv3k3BWdnQZMDi2hjfuA_76FVf_wA',
+  MAKE_WEBHOOK_URL: 'https://hook.eu1.make.com/h63v9tzv3o1i8hqtx2m5lfugrn5funy6',
+  CLAIM_EMAIL_RPC: 'claim_email_jobs_http'
+};
