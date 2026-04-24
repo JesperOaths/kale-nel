0f3a84bd6229f065bfe070337bfe763de8ba02a3
JesperOaths <150788310+JesperOaths@users.noreply.github.com>
2026-04-24 04:50:48 +0200

    Add files via upload

---
+++ gejast-shared-stats-config.js
@@ -0,0 +1,10 @@
+(function(){
+  window.GEJAST_CONFIG = Object.assign({}, window.GEJAST_CONFIG || {}, {
+    VERSION: 'v639',
+    SHARED_STATS_SUMMARY_RPC_V1: 'get_shared_stats_summary_public_v1',
+    SHARED_STATS_LEADERBOARD_RPC_V1: 'get_shared_stats_leaderboard_public_v1',
+    SHARED_STATS_ELO_HISTORY_RPC_V1: 'get_shared_elo_history_public_v1',
+    SHARED_STATS_MATCHUPS_RPC_V1: 'get_shared_matchups_public_v1',
+    ADMIN_SHARED_STATS_AUDIT_RPC_V1: 'admin_get_shared_stats_audit_v1'
+  });
+})();
